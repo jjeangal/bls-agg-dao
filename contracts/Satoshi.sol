8b4c4dabb56c4baf8f549560403e0114325d7d34
JEAN GAL <jgatbkk@gmail.com>
2024-04-08 10:44:36 -0400

    Satoshi contract for DAO to update satoshi guess

--- contracts/Satoshi.sol
+++ contracts/Satoshi.sol
@@ -2,10 +2,21 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.24;
 
-import "@openzeppelin/contracts/access/Ownable.sol";
+import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
 
 contract Satoshi is Ownable {
     address private _satoshi;
 
-    //event SatoshiFound(address indexed satoshi);
+    event SatoshiFound(address indexed satoshi);
+
+    constructor(address initialOwner) Ownable(initialOwner) {}
+
+    function updateSatoshi(address satoshi) external onlyOwner {
+        _satoshi = satoshi;
+        emit SatoshiFound(satoshi);
+    }
+
+    function getSatoshi() external view returns (address) {
+        return _satoshi;
+    }
 }
