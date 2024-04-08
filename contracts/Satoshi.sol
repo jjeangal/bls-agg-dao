05e40d8a2e0319593a963f5bec281bab0a1b6925
JEAN GAL <jgatbkk@gmail.com>
2024-04-07 21:17:18 -0400

    hardhat init

---
+++ contracts/Satoshi.sol
@@ -0,0 +1,11 @@
+// contracts/Satoshi.sol
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.24;
+
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract Satoshi is Ownable {
+    address private _satoshi;
+
+    //event SatoshiFound(address indexed satoshi);
+}
