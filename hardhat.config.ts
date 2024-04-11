f2e2bbd3e116ab64365bf9c7150aae16996fe976
JEAN GAL <jgatbkk@gmail.com>
2024-04-11 13:42:19 -0400

    new hardat config

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,8 +1,24 @@
 import "hardhat-deploy";
 import "@nomiclabs/hardhat-ethers";
 import "@typechain/hardhat";
+import { HardhatUserConfig } from "hardhat/config";
 
-/** @type import('hardhat/config').HardhatUserConfig */
-module.exports = {
+const config: HardhatUserConfig = {
+  defaultNetwork: "hardhat",
+  networks: {
+    hardhat: {
+      chainId: 1337 //31337?
+    },
+    localhost: {
+      chainId: 1337
+    }
+  },
   solidity: "0.8.24",
+  namedAccounts: {
+    deployer: {
+      default: 0,
+    },
+  },
 };
+
+export default config;
