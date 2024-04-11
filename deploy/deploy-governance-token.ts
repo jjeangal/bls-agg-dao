8d116e07c95caa90b3d8c37b7e08c58eb7d61537
JEAN GAL <jgatbkk@gmail.com>
2024-04-11 13:42:30 -0400

    get hre account, network, ...

--- deploy/deploy-governance-token.ts
+++ deploy/deploy-governance-token.ts
@@ -1,7 +1,10 @@
 import { HardhatRuntimeEnvironment } from 'hardhat/types';
 import { DeployFunction } from 'hardhat-deploy/types';
 
-const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
-    console.log("Get started!")
+const func: DeployFunction = async function (
+    hre: HardhatRuntimeEnvironment
+) {
+    const { getNamedAccounts, deployments, network } = hre;
+
 };
 export default func;
