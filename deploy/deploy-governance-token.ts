4f011f9a8cb93e46a6b4688cdbd755db187fd0d4
JEAN GAL <jgatbkk@gmail.com>
2024-04-11 13:36:57 -0400

    basic deploy governance token

---
+++ deploy/deploy-governance-token.ts
@@ -0,0 +1,7 @@
+import { HardhatRuntimeEnvironment } from 'hardhat/types';
+import { DeployFunction } from 'hardhat-deploy/types';
+
+const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
+    console.log("Get started!")
+};
+export default func;
