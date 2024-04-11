fdc813a7231d77f9abf11995ff4119b53b27fcc2
JEAN GAL <jgatbkk@gmail.com>
2024-04-11 13:33:08 -0400

    added imports to hardhat config

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,3 +1,7 @@
+import "hardhat-deploy";
+import "@nomiclabs/hardhat-ethers";
+import "@typechain/hardhat";
+
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.24",
