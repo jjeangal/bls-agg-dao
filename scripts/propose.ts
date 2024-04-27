70e1ae71adb6406235a0e3ab4186c70553ef576e
JEAN GAL <jgatbkk@gmail.com>
2024-04-27 18:33:43 -0400

    parsing proposal ids differently, through a function also changed the name of the contract called

--- scripts/propose.ts
+++ scripts/propose.ts
@@ -20,7 +20,7 @@ export async function propose(
     args: any[],
     proposalDescription: string
 ) {
-    const theGovernor = await ethers.getContractAt('Governor', theGovernorData.address);
+    const theGovernor = await ethers.getContractAt('TheGovernor', theGovernorData.address);
     const satoshi = await ethers.getContractAt('Satoshi', satoshiData.address);
 
     const encodedFunctionCall = satoshi.interface.encodeFunctionData(
@@ -43,18 +43,35 @@ export async function propose(
         await moveBlock(VOTING_DELAY + 1);
     }
 
-    const proposeReceipt = await proposeTx.wait(2);
+    const proposeReceipt = await proposeTx.wait(1);
     const proposalId = proposeReceipt.events[0].args.proposalId;
 
     console.log(`Proposed with proposal ID:\n  ${proposalId}`)
 
-    let proposals = JSON.parse(fs.readFileSync(proposalsFile, 'utf-8'));
+    const proposalState = await theGovernor.state(proposalId)
 
-    proposals[network.config.chainId!.toString()].push(proposalId.toString());
-    fs.writeFileSync(proposalsFile, JSON.stringify(proposals));
+    storeProposalId(proposalId);
+
+    // the Proposal State is an enum data type, defined in the IGovernor contract.
+    // 0:Pending, 1:Active, 2:Canceled, 3:Defeated, 4:Succeeded, 5:Queued, 6:Expired, 7:Executed
+    console.log(`Current Proposal State: ${proposalState}`)
 }
 
-propose(FUNCTION, [ADDRESS_SATOSHI], "")
+function storeProposalId(proposalId: any) {
+    const chainId = network.config.chainId!.toString();
+    let proposals: any;
+
+    if (fs.existsSync(proposalsFile)) {
+        proposals = JSON.parse(fs.readFileSync(proposalsFile, "utf8"));
+    } else {
+        proposals = {};
+        proposals[chainId] = [];
+    }
+    proposals[chainId].push(proposalId.toString());
+    fs.writeFileSync(proposalsFile, JSON.stringify(proposals), "utf8");
+}
+
+propose(FUNCTION, [ADDRESS_SATOSHI], PROPOSAL_DESCRIPTION)
     .then(() => {
         console.log("Proposed!");
         process.exit(0);
