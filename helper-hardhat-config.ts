12a8574fe3d128638bc0d4f8165a80684460d94c
JEAN GAL <jgatbkk@gmail.com>
2024-04-30 13:20:38 -0400

    slight quorum percentage update

--- helper-hardhat-config.ts
+++ helper-hardhat-config.ts
@@ -23,7 +23,7 @@ export const MIN_DELAY = 3600 // 1 hour - after a vote passes, you have 1 hour b
 
 export const VOTING_DELAY = 1 // Number of blocks till a proposal vote becomes active
 export const VOTING_PERIOD = 5 // Number or blocks
-export const QUORUM_PERCENTAGE = 3 // % of voters to pass
+export const QUORUM_PERCENTAGE = 4 // % of voters to pass
 
 export const ADDRESS_ZERO = "0x0000000000000000000000000000000000000000"
 export const ADDRESS_SATOSHI = "0x4df30AF0237E9a5c29D0f49a18Cb6f46692e3c71"
