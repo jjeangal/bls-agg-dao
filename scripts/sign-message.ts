d25231919194d935bb521b51e02a544445a81a94
JEAN GAL <jgatbkk@gmail.com>
2024-05-10 15:43:12 -0400

    call to contract BLS

--- scripts/sign-message.ts
+++ scripts/sign-message.ts
@@ -1,15 +1,29 @@
 import { ethers } from "hardhat"
 import blsData from "../deployments/localhost/BLS.json"
+import fs from "fs";
+import crypto from "crypto";
 
-export async function signProposalMessage(message: string, /**privateKey: string */): Promise<void> {
+import { KeyPair, keyPairsFile } from "../helper-crypto-config";
+
+export async function signProposalMessage(message: string, keyPairIndex: number): Promise<Uint8Array> {
 
     // Get BLS contract
-    const bls = await ethers.getContractAt(blsData.abi, blsData.address);
+    const blsContract = await ethers.getContractAt(blsData.abi, blsData.address);
 
-    let msgInBytes = ethers.utils.toUtf8Bytes(message);
-    const msgPoint = bls.hashToPoint(msgInBytes);
+    let msgInBytes: Uint8Array = ethers.utils.toUtf8Bytes(message);
+    const msgPoint: Uint8Array = blsContract.hashToPoint(msgInBytes);
 
     console.log(`Message Point: ${msgPoint}`);
 
     return msgPoint;
+}
+
+export async function getKeyPairs(): Promise<KeyPair[]> {
+    const keyPairs: KeyPair[] = JSON.parse(fs.readFileSync(keyPairsFile, 'utf8'));
+    return keyPairs;
+}
+
+export async function getKeyPairAtIndex(index: number): Promise<KeyPair> {
+    const keyPairs = await getKeyPairs();
+    return keyPairs[index];
 }
