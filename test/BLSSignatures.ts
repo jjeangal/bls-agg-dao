8e6e8f03082e50225304f08c9cb9f8c73f3da3d4
JEAN GAL <jgatbkk@gmail.com>
2024-05-12 23:02:05 -0400

    test for aggregate signatures

--- test/BLSSignatures.ts
+++ test/BLSSignatures.ts
@@ -51,4 +51,29 @@ describe("BLS Contract", function () {
         res = await BLSContract.verifySingle(sig, pk2, msg, { gasLimit: 80000000 });
         assert.strictEqual(res, false);
     });
+
+    it("Should perform a valid aggregate signature", async function () {
+
+        const message = PROPOSAL_DESCRIPTION;
+
+        const n = 10;
+        let messages = [];
+        let pubkeys = [];
+
+        let aggSignature = mcl.newG1();
+
+        for (let i = 0; i < n; i++) {
+            const { pubkey, secret } = mcl.newKeyPair();
+            const { signature, M } = mcl.sign(message, secret);
+            aggSignature = mcl.aggreagate(aggSignature, signature);
+            messages.push(M);
+            pubkeys.push(pubkey);
+        }
+
+        messages = messages.map((p) => mcl.g1ToBN(p));
+        pubkeys = pubkeys.map((p) => mcl.g2ToBN(p));
+        let sig_ser = mcl.g1ToBN(aggSignature);
+        let res = await BLSContract.verifyMultiple(sig_ser, pubkeys, messages);
+        assert.strictEqual(res, true);
+    });
 });
